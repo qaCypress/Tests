727cd822c2c7834e0cb732ba98160ab97d9f1558
Ростислав Решетар <u1-911@office107.local>
2023-07-20 14:26:31 +0300

    s

--- cypress/e2e/first_daily.cy.js
+++ cypress/e2e/first_daily.cy.js
@@ -1,6 +1,9 @@
 describe('sho', () => {
     it('sds', () => {
-      cy.intercept('https://slottica.com/ru', { statusCode: 200, body: 'Mocked response' })
-      cy.visit('https://slottica.com/ru')
+      cy.visit('https://slottica.com/ru', {
+        failOnStatusCode: false
+      })
+
+      cy.get('.message').contains('Приносим')
     })
   })
